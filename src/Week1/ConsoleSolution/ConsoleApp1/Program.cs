6c606520762053f77e824f24aa5dea9ee90ea5fe
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-18 15:58:05 -0400

    Ship the API!

---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+
+app.MapGet("/message", () =>
+{
+    var response = new MessageResponseModel("This is an API! Wow!", DateTime.Now);
+    return Results.Ok(response);
+});
+
+
+app.Run();
+
+
+public record MessageResponseModel(string Message, DateTime When);
