d0233e48cfc6227400ad169c97383d41909f8edd
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-19 16:33:23 -0400

    done with day 2

--- src/Week1/BankingSolution/Banking.Domain/Account.cs
+++ src/Week1/BankingSolution/Banking.Domain/Account.cs
@@ -3,18 +3,18 @@
 public class Account
 {
     private decimal _balance = 5000M;
-    public void Deposit(decimal amountToDeposit)
+    public void Deposit(TransactionValueTypes.Deposit amountToDeposit)
     {
         _balance += amountToDeposit;
     }
 
-    public decimal GetBalance()
+    public TransactionValueTypes.Balance GetBalance()
     {
 
         return _balance;
     }
-
-    public void Withdraw(decimal amountToWithdraw)
+    // "Primitive Obsession" -
+    public void Withdraw(TransactionValueTypes.Withdrawl amountToWithdraw)
     {
         GuardHasSufficientFunds(amountToWithdraw);
 
