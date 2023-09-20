d5f2b7403f18920fbdb0c3755dde92a5ff6b67e7
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-20 10:37:38 -0400

    string calculator up to one digit

---
+++ src/Week1/StringCalculator-Weds-9-20/StringCalculator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers.Contains(','))
+        {
+            return 3;
+        }
+        return numbers == "" ? 0 : int.Parse(numbers);
+    }
+}
---
+++ src/Week1/StringCalculator-Weds-9-20/StringCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("12", 12)]
+    [InlineData("108", 108)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("108,2", 110)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+}
