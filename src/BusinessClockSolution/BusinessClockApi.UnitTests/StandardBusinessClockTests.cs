74798ea1cc191bb33fcb9a9e04c3e6a160a1e4d0
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-22 15:34:42 -0400

    Failing Test Needs Fixed

--- src/BusinessClockSolution/BusinessClockApi.UnitTests/StandardBusinessClockTests.cs
+++ src/BusinessClockSolution/BusinessClockApi.UnitTests/StandardBusinessClockTests.cs
@@ -4,10 +4,8 @@ namespace BusinessClockApi.UnitTests;
 public class StandardBusinessClockTests
 {
     [Theory]
-    [InlineData("9/22/2023 8:59:00 AM", "9/25/2023 9:00:00 AM")]
-
+    [InlineData("9/22/2023 8:59:00 AM", "9/22/2023 9:00:00 AM")]
     [InlineData("9/21/2023 5:00:00 PM", "9/22/2023 9:00:00 AM")]
-
     public void Closed(string currentTime, string openNext)
     {
         var fakeSystemTime = Substitute.For<ISystemTime>();
@@ -24,7 +22,6 @@ public class StandardBusinessClockTests
     [Theory]
     [InlineData("9/22/2023 9:00:00 AM")]
     [InlineData("9/22/2023 4:59:59 PM")]
-
     public void Open(string currentTime)
     {
         var fakeSystemTime = Substitute.For<ISystemTime>();
