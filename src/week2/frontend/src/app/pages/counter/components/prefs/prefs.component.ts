45e0c5b8ea603927f16e2e63640d0384d0ad6eae
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-27 13:38:29 -0400

    count by disabled

--- src/week2/frontend/src/app/pages/counter/components/prefs/prefs.component.ts
+++ src/week2/frontend/src/app/pages/counter/components/prefs/prefs.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { CountByValues, CounterEvents } from '../../state/counter.actions';
+import { counterFeature } from '../../state';
 
 @Component({
   selector: 'app-prefs',
@@ -9,9 +10,9 @@ import { CountByValues, CounterEvents } from '../../state/counter.actions';
   imports: [CommonModule],
   template: `
     <div class="join">
-  <button (click)="setCountBy(1)" class="btn join-item">Count By 1</button>
-  <button (click)="setCountBy(3)" class="btn join-item">Count By 3</button>
-  <button (click)="setCountBy(5)" class="btn join-item">Count By 5</button>
+  <button [disabled]="by() === 1" (click)="setCountBy(1)" class="btn join-item">Count By 1</button>
+  <button [disabled]="by() === 3" (click)="setCountBy(3)" class="btn join-item">Count By 3</button>
+  <button [disabled]="by() === 5" (click)="setCountBy(5)" class="btn join-item">Count By 5</button>
 </div>
   `,
   styles: [
@@ -19,7 +20,7 @@ import { CountByValues, CounterEvents } from '../../state/counter.actions';
 })
 export class PrefsComponent {
   store = inject(Store);
-
+  by = this.store.selectSignal(counterFeature.selectBy);
   setCountBy(by: CountByValues) {
     this.store.dispatch(CounterEvents.countByChanged({ by }));
   }
