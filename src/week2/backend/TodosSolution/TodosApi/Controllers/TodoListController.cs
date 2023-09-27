f39a9b05e5950dd8a5707c28ce3631e897b279d6
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-27 15:50:46 -0400

    project created

---
+++ src/week2/backend/TodosSolution/TodosApi/Controllers/TodoListController.cs
@@ -0,0 +1,21 @@
+﻿namespace TodosApi.Controllers;
+
+[ApiController]
+public class TodoListController : ControllerBase
+{
+
+    // GET /todo-list
+
+    [HttpPost("/todo-list")]
+    public async Task<ActionResult> AddTodoItem([FromBody] TodoCreateRequest request)
+    {
+        // Some Magic! (Code)
+        var response = new TodoItemResponse
+        {
+            Id = Guid.NewGuid().ToString(),
+            Description = request.Description,
+            Completed = false
+        };
+        return StatusCode(201, response);
+    }
+}
---
+++ src/week2/backend/TodosSolution/TodosApi/Models/TodosModels.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodosApi.Models;
+
+
+public record TodoCreateRequest
+{
+    [Required, MaxLength(255)]
+    public string Description { get; set; } = string.Empty;
+}
+
+public record TodoItemResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool Completed { get; set; }
+}
