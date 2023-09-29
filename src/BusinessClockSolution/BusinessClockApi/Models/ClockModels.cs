1ed8010961bfb5ce122eea8bc7bbef5252a3c302
Jeff Gonzalez <jeff@hypertheory.com>
2023-09-29 16:08:09 -0400

    :shipit:

--- src/BusinessClockSolution/BusinessClockApi/Models/ClockModels.cs
+++ src/BusinessClockSolution/BusinessClockApi/Models/ClockModels.cs
@@ -4,6 +4,7 @@ public record ClockResponse
 {
     public bool Open { get; init; }
     public DateTime? OpensNext { get; init; } = null;
+    public string SecretMessage { get; } = "Jeff Was Here";
 
     public static ClockResponse CreateOpen() => new ClockResponse { Open = true };
     public static ClockResponse CreateClosed(DateTime whenOpen) => new ClockResponse { Open = false, OpensNext = whenOpen };
